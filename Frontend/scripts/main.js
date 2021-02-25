4520c5a3a3bcd5ba07bd6d5f65aa879336cdca2c
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-02-25 15:59:15 +0200

    organized folders

---
+++ Frontend/scripts/main.js
@@ -0,0 +1,22 @@
+const cards = createDeck();
+const newDeckNumber = 54;
+
+const deck = new Deck(cards, newDeckNumber);
+
+// console.log(deck);
+// deck.shuffle();
+// console.log(deck);
+
+//creates a new deck
+function createDeck() {
+  const deck = new Array();
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+      deck.push(new Card(suits[i], values[x], false));
+    }
+  }
+  deck.push(new Card(null, 0, true));
+  deck.push(new Card(null, 0, true));
+  return deck;
+}
