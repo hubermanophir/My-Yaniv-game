45363b0cea71e3ed8bb0dafea1cfe3e2f21d79b8
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-02-25 22:13:39 +0200

    fixed decks of cards locations

--- Frontend/scripts/main.js
+++ Frontend/scripts/main.js
@@ -1,23 +1,35 @@
 const selection = document.getElementById("player-number");
 const start = document.getElementById("start");
 const pileDeck = document.getElementById("pile-deck");
+const player1Name = document.getElementById("name1");
+const player2Name = document.getElementById("name2");
+const player3Name = document.getElementById("name3");
+const player4Name = document.getElementById("name4");
+const playerNames = [player1Name, player2Name, player3Name, player4Name];
+const player1Deck = document.getElementById("player1-deck");
+const player2Deck = document.getElementById("player2-deck");
+const player3Deck = document.getElementById("player3-deck");
+const player4Deck = document.getElementById("player4-deck");
+const playerDecks = [player1Deck, player2Deck, player3Deck, player4Deck];
+
 let numberOfPlayers;
 const tableDeck = createTableDeck();
 const players = [];
 
+//starting the game
 start.addEventListener("click", () => {
   numberOfPlayers = selection.value;
   createPlayers();
-  createCardDiv(tableDeck.cards.pop());
-  // console.log(players);
+  createCardDiv(tableDeck.cards.pop(), pileDeck);
+  console.log(players);
   start.hidden = true;
   console.log(tableDeck);
+  setCardsToPlayers(players);
 });
 
 //creates a new deck
 function createDeck() {
   const deck = new Array();
-
   for (let i = 0; i < suits.length; i++) {
     for (let x = 0; x < values.length; x++) {
       deck.push(new Card(suits[i], values[x], false));
@@ -44,13 +56,15 @@ function createPlayers() {
     for (let x = 0; x < 5; x++) {
       cards.push(tableDeck.cards.pop());
     }
-    const player = new Player(prompt(`player ${i + 1} name`), cards);
+    const name = prompt(`player ${i + 1} name`);
+    const player = new Player(name, cards);
+    playerNames[i].innerText = name;
     players.push(player);
   }
 }
 
 //creating a card div from card object
-function createCardDiv(card) {
+function createCardDiv(card, parent) {
   const img = document.createElement("img");
   const { suit } = card;
   const { rank } = card;
@@ -58,7 +72,7 @@ function createCardDiv(card) {
   let src;
   if (isJoker) {
     img.setAttribute("id", `joker-card`);
-    img.setAttribute("src", `../images/card-fronts/joker`);
+    img.setAttribute("src", `../images/card-fronts/joker.png`);
   } else {
     switch (suit) {
       case "hearts":
@@ -77,6 +91,18 @@ function createCardDiv(card) {
     img.setAttribute("id", `${suit}_${rank}`);
     img.setAttribute("src", `${src}`);
   }
-  img.style.height = "150px";
-  pileDeck.appendChild(img);
+  img.style.height = "115px";
+  parent.appendChild(img);
+}
+
+//adding the cards to the corresponding player
+function setCardsToPlayers(players) {
+  let counter = 0;
+  for (const player of players) {
+    const cards = player.playerDeck;
+    for (let i = 0; i < 5; i++) {
+      createCardDiv(cards[i], playerDecks[counter]);
+    }
+    counter++;
+  }
 }
