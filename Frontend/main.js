b52ead2d426587fa8092329e31a228b921a1b905
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-02-25 14:59:24 +0200

    created a createDeck function

--- Frontend/main.js
+++ Frontend/main.js
@@ -12,3 +12,39 @@ class Deck {
     this.cards = deck;
   }
 }
+const suits = ["spades", "diamonds", "clubs", "hearts"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+const cards = createDeck();
+const newDeckNumber = 54;
+
+const deck = new Deck(cards, newDeckNumber);
+
+console.log(deck);
+
+function createDeck() {
+  const deck = new Array();
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+      deck.push(new Card(suits[i], values[x], false));
+    }
+  }
+  deck.push(new Card(null, 0, true));
+  deck.push(new Card(null, 0, true));
+  return deck;
+}
